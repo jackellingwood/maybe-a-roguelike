93ef0a7707949450f48f24c2b2d5736c63d98577
jackellingwood <jack.ellingwood@baxter-academy.org>
2023-02-13 13:33:03 -0500

    onto pt 2 of tcod tutorial

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
-from cell import *
-from thing import *
-from player import *
-from game import *
-game = Game()
-x = 4
-y = 3
-game.screen[x][y].add(Player(x, y, []))
-game.printAsScreen()
-print(game.screen[x][y].contents[0].getInv())
-# for y in range(5):
-#     screen.append([])
-#     for x in range(10):
-#         screen[y].append(Cell(x, y, []))
+import tcod
 
-# ourCell = screen[4][3]
-# ourCell.add(thing(2))
-# print(ourCell)
-# print("    ┌─┐   ┌─┐\n    │ │   │ │\n    └─┘   └─┘\n ┌─────────────┐\n └┐           ┌┘\n  └───────────┘ ")
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    event_handler = EventHandler()
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Yet Another Roguelike Tutorial",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=player_x, y=player_y, string="@")
+
+            context.present(root_console)
+
+            root_console.clear()
+
+            for event in tcod.event.wait():
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit()
+
+
+if __name__ == "__main__":
+    main()
